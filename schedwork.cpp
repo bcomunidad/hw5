c88f5ddd07f31020781a1fb4e86bfd28bef5bf0b
Bryant Comunidad <comunida@usc.edu>
2024-04-20 23:02:41 -0700

    finished hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,19 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool canSchedule(const AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    std::vector<unsigned int> shiftsCount,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    const size_t day,
+    const size_t workerIdx,
+    const size_t currentNeed);
 
+void resizeSchedule(const AvailabilityMatrix& avail, const size_t dailyNeed, DailySchedule& sched);
+void resizeShiftsCount(const AvailabilityMatrix& avail, std::vector<unsigned int>& shiftsCount);
+bool isValid(const AvailabilityMatrix& avail, int index, const size_t day, std::vector<unsigned int> shiftsCount, const size_t maxShifts);
+bool baseCase(const AvailabilityMatrix& avail, const size_t day);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +49,43 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    if(avail[0].size() == 0) {
+        return false;
+    }
+    sched.resize(avail.size(), std::vector<Worker_T>(dailyNeed));
+    std::vector<unsigned int> shiftsCount;
+    shiftsCount.resize(avail[0].size(), 0);
+    return canSchedule(avail, sched, shiftsCount, dailyNeed, maxShifts, 0, 0, 0);
+}
 
+bool canSchedule(const AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    std::vector<unsigned int> shiftsCount,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    const size_t day,
+    const size_t workerIdx,
+    const size_t currentNeed) {
+        if(baseCase(avail, day)) return true;
+        if(currentNeed == dailyNeed) return canSchedule(avail, sched, shiftsCount, dailyNeed, maxShifts, day+1, 0, 0);
+        for(size_t i = workerIdx; i < avail[0].size(); i++) {
+            if(isValid(avail, i, day, shiftsCount, maxShifts)) {
+                sched[day][currentNeed] = i;
+                shiftsCount[i] += 1;
+                if(canSchedule(avail, sched, shiftsCount, dailyNeed, maxShifts, day, i+1, currentNeed+1)) {
+                    return true;
+                }
+                shiftsCount[i] -= 1;
+            }
+        }
+        return false;
+    }
 
+bool isValid(const AvailabilityMatrix& avail, int index, const size_t day, std::vector<unsigned int> shiftsCount, const size_t maxShifts) {
+    return (avail[day][index]) && (shiftsCount[index] < maxShifts);
+}
 
-
+bool baseCase(const AvailabilityMatrix& avail, const size_t day) {
+    return (day == avail.size());
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,16 +14,54 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void permutations(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::string word, std::set<std::string>& results, unsigned index);
+bool isValid(const std::string& in, const std::string& floating, std::string word);
 
 // Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
-
+std::set<std::string> wordle(const std::string& in, const std::string& floating, const std::set<std::string>& dict) {
+    std::set<std::string> results;
+    std::string word = in;
+    permutations(in, floating, dict, word, results, 0);
+    return results;
 }
 
 // Define any helper functions here
+
+void permutations(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::string word, std::set<std::string>& results, unsigned index) {
+    if(index == in.length()) {
+        if(dict.find(word) != dict.end()) {
+            if(isValid(in, floating, word)) {
+                results.insert(word);
+            }
+        }
+        return;
+    }
+    if(in[index] == '-') {
+        for(char i = 'a'; i <= 'z'; i++) {
+            word[index] = i;
+            permutations(in, floating, dict, word, results, index+1);
+        }
+    }
+    else {
+        word[index] = in[index];
+        permutations(in, floating, dict, word, results, index+1);
+    }
+}
+
+bool isValid(const std::string& in, const std::string& floating, std::string word) {
+    for(unsigned int i = 0; i < in.length(); i++) {
+        if(in[i] != word[i] && in[i] != '-') {
+            return false;
+        }
+    } 
+    for(unsigned int i = 0; i < floating.size(); i++) {
+        char c = floating[i];
+        size_t pos = word.find(c);
+        if(pos == std::string::npos) {
+            return false;
+        }
+        word[pos] = '*';
+    }
+    return true;
+}
+
